bf73666ff953d7ed148d64d1d06459bbb24ed2b1
Nassim Sahib <nassim.sahib775@gmail.com>
2026-09-23 01:39:04 +0200

    Log returns with numeric algorithms and ranges

---
+++ LogReturns.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <format>
+
+#include <vector>
+#include <deque>
+#include <ranges>
+
+#include <algorithm>
+#include <numeric>
+
+#include <functional>
+
+int main()
+{
+	std::vector<double> prices
+	{
+	25.5, 28.0, 30.5, 31.0, 27.5, 31.0, 29.5, 28.5, 37.5,
+	33.5, 25.5, 31.5, 26.5, 29.5, 32.5, 34.5, 28.5, 35.5,
+	28.5, 29.0, 32.0, 23.5, 27.5, 33.5, 28.0, 28.0, 32.5,
+	31.5, 29.0, 33.0, 32.5, 29.5, 34.5
+	};
+	// First / Intuition Method
+	std::adjacent_difference(prices.begin(), prices.end(), prices.begin(), std::divides<double>());
+	std::deque<double> log_rtrns(prices.size());
+
+	std::ranges::transform(prices.begin(), prices.end(), log_rtrns.begin(), [](double x) 
+		{
+		return std::log(x);
+		});
+	log_rtrns.pop_front();
+
+	for (double log_rt : log_rtrns) {
+		std::cout << log_rt << " ";
+	}
+}
--- VWAP.cpp
+++ VWAP.cpp
@@ -6,26 +6,26 @@
 #include <algorithm>
 #include <numeric>
 
-
-int main()
-{
-	// Volumes and Prices between 10 AM to 04 PM (30 minutes Intervals)
-	std::vector v
-	{
-	376000, 365000, 344000, 346000, 345000, 336000, 335000,
-	339000, 340000, 340000, 343000, 367000, 37400
-	};
-	std::vector p
-	{
-	208.59, 206.93, 207.75, 209.21, 208.58, 208.63, 207.92,
-	208.87, 208.16, 209.49, 208.53, 209.12, 209.05
-	};
-
-	double avg_day_volume = std::accumulate(v.begin(), v.end(), 0.0);
-	double raw_wgt_price = std::inner_product(v.begin(), v.end(), p.begin(), 0.0);
-
-	double VWAP = raw_wgt_price / avg_day_volume;
-
-	std::cout << std::format("VWAP for the day is {}", VWAP);
-
-}
+//
+//int main()
+//{
+//	// Volumes and Prices between 10 AM to 04 PM (30 minutes Intervals)
+//	std::vector v
+//	{
+//	376000, 365000, 344000, 346000, 345000, 336000, 335000,
+//	339000, 340000, 340000, 343000, 367000, 37400
+//	};
+//	std::vector p
+//	{
+//	208.59, 206.93, 207.75, 209.21, 208.58, 208.63, 207.92,
+//	208.87, 208.16, 209.49, 208.53, 209.12, 209.05
+//	};
+//
+//	double avg_day_volume = std::accumulate(v.begin(), v.end(), 0.0);
+//	double raw_wgt_price = std::inner_product(v.begin(), v.end(), p.begin(), 0.0);
+//
+//	double VWAP = raw_wgt_price / avg_day_volume;
+//
+//	std::cout << std::format("VWAP for the day is {}", VWAP);
+//
+//}
