d5df364d3fa833a37bce2c787d3e7c650d5d8e75
Nassim Sahib <nassim.sahib775@gmail.com>
2026-09-24 09:35:18 +0200

    Monte Carlo Barrier Option Pricing

---
+++ BarrierType.h
@@ -0,0 +1,8 @@
+#pragma once
+
+enum class BarrierType
+{
+	none,
+	up_and_out,
+	down_and_out
+};
--- MCOptionValuation.h
+++ MCOptionValuation.h
@@ -1,12 +1,16 @@
 #pragma once
 
+
 #include "OptionInfo.h"
+#include "BarrierType.h"
 
 class MCOptionValuation
 {
 public:
 	MCOptionValuation(OptionInfo&& opt, int time_steps,
-		double vol, double int_rate, double div_rate = 0.0);
+		double vol, double int_rate, double div_rate = 0.0,
+		BarrierType barrier_type = BarrierType::none, 
+		double barrier_value = 0.0);
 
 	double calc_price(double spot, int num_scenarios,
 		unsigned unif_start_seed);
@@ -14,4 +18,6 @@ private:
 	OptionInfo opt_;
 	unsigned time_steps_;
 	double vol_, int_rate_, div_rate_;
+	BarrierType barrier_type_;
+	double barrier_value_;
 };
